c8e08bf1b1798bde5ee2d1b9db010867248dd548
dr.john.lindsay <dr.john.lindsay@gmail.com>
2012-05-04 13:52:44 +0000

    Raster math related plugin tools

---
+++ MathTools/src/plugins/Cosh.java
@@ -0,0 +1,195 @@
+/*
+ * Copyright (C) 2011 Dr. John Lindsay <jlindsay@uoguelph.ca>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package plugins;
+
+import java.util.Date;
+import whitebox.geospatialfiles.WhiteboxRaster;
+import whitebox.interfaces.WhiteboxPluginHost;
+import whitebox.interfaces.WhiteboxPlugin;
+
+/**
+ *
+ * @author Dr. John Lindsay <jlindsay@uoguelph.ca>
+ */
+public class Cosh implements WhiteboxPlugin {
+    
+    private WhiteboxPluginHost myHost = null;
+    private String[] args;
+
+    @Override
+    public String getName() {
+        return "Cosh";
+    }
+
+    @Override
+    public String getDescriptiveName() {
+    	return "Cosh";
+    }
+
+    @Override
+    public String getToolDescription() {
+    	return "Returns the hyperbolic cosine of each values in a raster.";
+    }
+
+    @Override
+    public String[] getToolbox() {
+    	String[] ret = { "TrigFun" };
+    	return ret;
+    }
+
+    @Override
+    public void setPluginHost(WhiteboxPluginHost host) {
+        myHost = host;
+    }
+
+    private void showFeedback(String message) {
+        if (myHost != null) {
+            myHost.showFeedback(message);
+        } else {
+            System.out.println(message);
+        }
+    }
+
+    private void returnData(Object ret) {
+        if (myHost != null) {
+            myHost.returnData(ret);
+        }
+    }
+
+    private int previousProgress = 0;
+    private String previousProgressLabel = "";
+    private void updateProgress(String progressLabel, int progress) {
+        if (myHost != null && ((progress != previousProgress) || 
+                (!progressLabel.equals(previousProgressLabel)))) {
+            myHost.updateProgress(progressLabel, progress);
+        }
+        previousProgress = progress;
+        previousProgressLabel = progressLabel;
+    }
+
+    private void updateProgress(int progress) {
+        if (myHost != null && progress != previousProgress) {
+            myHost.updateProgress(progress);
+        }
+        previousProgress = progress;
+    }
+    
+    @Override
+    public void setArgs(String[] args) {
+        this.args = args.clone();
+    }
+    
+    private boolean cancelOp = false;
+    @Override
+    public void setCancelOp(boolean cancel) {
+        cancelOp = cancel;
+    }
+    
+    private void cancelOperation() {
+        showFeedback("Operation cancelled.");
+        updateProgress("Progress: ", 0);
+    }
+    
+    private boolean amIActive = false;
+    @Override
+    public boolean isActive() {
+        return amIActive;
+    }
+
+    @Override
+    public void run() {
+        amIActive = true;
+        
+        String inputHeader = null;
+        String outputHeader = null;
+    	
+        if (args.length <= 0) {
+            showFeedback("Plugin parameters have not been set.");
+            return;
+        }
+        
+        for (int i = 0; i < args.length; i++) {
+            if (i == 0) {
+                inputHeader = args[i];
+            } else if (i == 1) {
+                outputHeader = args[i];
+            }
+        }
+
+        // check to see that the inputHeader and outputHeader are not null.
+        if ((inputHeader == null) || (outputHeader == null)) {
+            showFeedback("One or more of the input parameters have not been set properly.");
+            return;
+        }
+
+        try {
+            int row, col;
+            double z;
+            float progress = 0;
+            int numCells = 0;
+            double[] data;
+            
+            WhiteboxRaster inputFile = new WhiteboxRaster(inputHeader, "r");
+
+            int rows = inputFile.getNumberRows();
+            int cols = inputFile.getNumberColumns();
+            
+            double noData = inputFile.getNoDataValue();
+            
+            double multiplier = Math.PI / 180;
+            if (inputFile.getZUnits().toLowerCase().contains("rad")) { // radians
+                multiplier = 1;
+            }
+
+            WhiteboxRaster outputFile = new WhiteboxRaster(outputHeader, "rw", inputHeader, WhiteboxRaster.DataType.FLOAT, noData);
+            outputFile.setPreferredPalette(inputFile.getPreferredPalette());
+
+            for (row = 0; row < rows; row++) {
+                data = inputFile.getRowValues(row);
+                for (col = 0; col < cols; col++) {
+                    z = data[col];
+                    if (z != noData) {
+                        outputFile.setValue(row, col, Math.cosh(z * multiplier));
+                    }
+
+                }
+                if (cancelOp) { cancelOperation(); return; }
+                progress = (float) (100f * row / (rows - 1));
+                updateProgress((int) progress);
+            }
+
+            outputFile.addMetadataEntry("Created by the "
+                    + getDescriptiveName() + " tool.");
+            outputFile.addMetadataEntry("Created on " + new Date());
+
+            // close all of the open Whitebox rasters.
+            inputFile.close();
+            outputFile.close();
+
+            // returning a header file string displays the image.
+            returnData(outputHeader);
+            
+        } catch (Exception e) {
+            showFeedback(e.getMessage());
+        } finally {
+            updateProgress("Progress: ", 0);
+            // tells the main application that this process is completed.
+            amIActive = false;
+            myHost.pluginComplete();
+        }
+    }
+}
