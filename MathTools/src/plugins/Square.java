78b22073a5a7b28b4b55768f48ddaed84b5dad30
nirkas <nirkas@gmail.com>
2012-05-17 19:38:44 +0000

    basic documentation added for MathTools package

--- MathTools/src/plugins/Square.java
+++ MathTools/src/plugins/Square.java
@@ -19,10 +19,11 @@ package plugins;
 import java.io.File;
 import java.util.Date;
 import whitebox.geospatialfiles.WhiteboxRaster;
-import whitebox.interfaces.WhiteboxPluginHost;
 import whitebox.interfaces.WhiteboxPlugin;
+import whitebox.interfaces.WhiteboxPluginHost;
 
 /**
+ * WhiteboxPlugin is used to define a plugin tool for Whitebox GIS.
  *
  * @author Dr. John Lindsay <jlindsay@uoguelph.ca>
  */
@@ -31,32 +32,67 @@ public class Square implements WhiteboxPlugin {
     private WhiteboxPluginHost myHost = null;
     private String[] args;
 
+    /**
+     * Used to retrieve the plugin tool's name. This is a short, unique name
+     * containing no spaces.
+     *
+     * @return String containing plugin name.
+     */
     @Override
     public String getName() {
         return "Square";
     }
 
+    /**
+     * Used to retrieve the plugin tool's descriptive name. This can be a longer
+     * name (containing spaces) and is used in the interface to list the tool.
+     *
+     * @return String containing the plugin descriptive name.
+     */
     @Override
     public String getDescriptiveName() {
         return "Square";
     }
 
+    /**
+     * Used to retrieve a short description of what the plugin tool does.
+     *
+     * @return String containing the plugin's description.
+     */
     @Override
     public String getToolDescription() {
         return "Squares the values in a raster.";
     }
 
+    /**
+     * Used to identify which toolboxes this plugin tool should be listed in.
+     *
+     * @return Array of Strings.
+     */
     @Override
     public String[] getToolbox() {
         String[] ret = {"MathTools"};
         return ret;
     }
 
+    /**
+     * Sets the WhiteboxPluginHost to which the plugin tool is tied. This is the
+     * class that the plugin will send all feedback messages, progress updates,
+     * and return objects.
+     *
+     * @param host The WhiteboxPluginHost that called the plugin tool.
+     */
     @Override
     public void setPluginHost(WhiteboxPluginHost host) {
         myHost = host;
     }
 
+    /**
+     * Used to communicate feedback pop-up messages between a plugin tool and
+     * the main Whitebox user-interface.
+     *
+     * @param feedback String containing the text to display.
+     */
     private void showFeedback(String message) {
         if (myHost != null) {
             myHost.showFeedback(message);
@@ -65,6 +101,12 @@ public class Square implements WhiteboxPlugin {
         }
     }
 
+    /**
+     * Used to communicate a return object from a plugin tool to the main
+     * Whitebox user-interface.
+     *
+     * @return Object, such as an output WhiteboxRaster.
+     */
     private void returnData(Object ret) {
         if (myHost != null) {
             myHost.returnData(ret);
@@ -73,6 +115,13 @@ public class Square implements WhiteboxPlugin {
     private int previousProgress = 0;
     private String previousProgressLabel = "";
 
+    /**
+     * Used to communicate a progress update between a plugin tool and the main
+     * Whitebox user interface.
+     *
+     * @param progressLabel A String to use for the progress label.
+     * @param progress Float containing the progress value (between 0 and 100).
+     */
     private void updateProgress(String progressLabel, int progress) {
         if (myHost != null && ((progress != previousProgress)
                 || (!progressLabel.equals(previousProgressLabel)))) {
@@ -82,6 +131,12 @@ public class Square implements WhiteboxPlugin {
         previousProgressLabel = progressLabel;
     }
 
+    /**
+     * Used to communicate a progress update between a plugin tool and the main
+     * Whitebox user interface.
+     *
+     * @param progress Float containing the progress value (between 0 and 100).
+     */
     private void updateProgress(int progress) {
         if (myHost != null && progress != previousProgress) {
             myHost.updateProgress(progress);
@@ -89,12 +144,22 @@ public class Square implements WhiteboxPlugin {
         previousProgress = progress;
     }
 
+    /**
+     * Sets the arguments (parameters) used by the plugin.
+     *
+     * @param args
+     */
     @Override
     public void setArgs(String[] args) {
         this.args = args.clone();
     }
     private boolean cancelOp = false;
 
+    /**
+     * Used to communicate a cancel operation from the Whitebox GUI.
+     *
+     * @param cancel Set to true if the plugin should be canceled.
+     */
     @Override
     public void setCancelOp(boolean cancel) {
         cancelOp = cancel;
@@ -106,6 +171,12 @@ public class Square implements WhiteboxPlugin {
     }
     private boolean amIActive = false;
 
+   /**
+     * Used by the Whitebox GUI to tell if this plugin is still running.
+     *
+     * @return a boolean describing whether or not the plugin is actively being
+     * used.
+     */
     @Override
     public boolean isActive() {
         return amIActive;
