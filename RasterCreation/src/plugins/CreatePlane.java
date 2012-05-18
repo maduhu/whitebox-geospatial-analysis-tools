eda86d03d267b232fd4902a28f67de0c521d8c3a
nirkas <nirkas@gmail.com>
2012-05-18 19:17:02 +0000

    basic documentation added for RasterCreation package

--- RasterCreation/src/plugins/CreatePlane.java
+++ RasterCreation/src/plugins/CreatePlane.java
@@ -22,6 +22,7 @@ import whitebox.interfaces.WhiteboxPlugin;
 import whitebox.interfaces.WhiteboxPluginHost;
 
 /**
+ * WhiteboxPlugin is used to define a plugin tool for Whitebox GIS.
  *
  * @author Dr. John Lindsay <jlindsay@uoguelph.ca>
  */
@@ -33,35 +34,66 @@ public class CreatePlane implements WhiteboxPlugin {
     private static final double LnOf2 = 0.693147180559945;
 
     /**
-     * Returns the name of this tool.
-     * @return String containing tool name.
+     * Used to retrieve the plugin tool's name. This is a short, unique name
+     * containing no spaces.
+     *
+     * @return String containing plugin name.
      */
     @Override
     public String getName() {
         return "CreatePlane";
     }
 
+    /**
+     * Used to retrieve the plugin tool's descriptive name. This can be a longer
+     * name (containing spaces) and is used in the interface to list the tool.
+     *
+     * @return String containing the plugin descriptive name.
+     */
     @Override
     public String getDescriptiveName() {
         return "Create Plane";
     }
 
+    /**
+     * Used to retrieve a short description of what the plugin tool does.
+     *
+     * @return String containing the plugin's description.
+     */
     @Override
     public String getToolDescription() {
         return "Creates a raster image based on the equation for a simple plane.";
     }
 
+    /**
+     * Used to identify which toolboxes this plugin tool should be listed in.
+     *
+     * @return Array of Strings.
+     */
     @Override
     public String[] getToolbox() {
         String[] ret = {"RasterCreation"};
         return ret;
     }
 
+    /**
+     * Sets the WhiteboxPluginHost to which the plugin tool is tied. This is the
+     * class that the plugin will send all feedback messages, progress updates,
+     * and return objects.
+     *
+     * @param host The WhiteboxPluginHost that called the plugin tool.
+     */
     @Override
     public void setPluginHost(WhiteboxPluginHost host) {
         myHost = host;
     }
 
+    /**
+     * Used to communicate feedback pop-up messages between a plugin tool and
+     * the main Whitebox user-interface.
+     *
+     * @param feedback String containing the text to display.
+     */
     private void showFeedback(String message) {
         if (myHost != null) {
             myHost.showFeedback(message);
@@ -70,6 +102,12 @@ public class CreatePlane implements WhiteboxPlugin {
         }
     }
 
+     /**
+     * Used to communicate a return object from a plugin tool to the main
+     * Whitebox user-interface.
+     *
+     * @return Object, such as an output WhiteboxRaster.
+     */
     private void returnData(Object ret) {
         if (myHost != null) {
             myHost.returnData(ret);
@@ -78,6 +116,13 @@ public class CreatePlane implements WhiteboxPlugin {
     private int previousProgress = 0;
     private String previousProgressLabel = "";
 
+    /**
+     * Used to communicate a progress update between a plugin tool and the main
+     * Whitebox user interface.
+     *
+     * @param progressLabel A String to use for the progress label.
+     * @param progress Float containing the progress value (between 0 and 100).
+     */
     private void updateProgress(String progressLabel, int progress) {
         if (myHost != null && ((progress != previousProgress)
                 || (!progressLabel.equals(previousProgressLabel)))) {
@@ -87,6 +132,12 @@ public class CreatePlane implements WhiteboxPlugin {
         previousProgressLabel = progressLabel;
     }
 
+    /**
+     * Used to communicate a progress update between a plugin tool and the main
+     * Whitebox user interface.
+     *
+     * @param progress Float containing the progress value (between 0 and 100).
+     */
     private void updateProgress(int progress) {
         if (myHost != null && progress != previousProgress) {
             myHost.updateProgress(progress);
@@ -94,12 +145,22 @@ public class CreatePlane implements WhiteboxPlugin {
         previousProgress = progress;
     }
 
+    /**
+     * Sets the arguments (parameters) used by the plugin.
+     *
+     * @param args
+     */
     @Override
     public void setArgs(String[] args) {
         this.args = args.clone();
     }
     private boolean cancelOp = false;
 
+    /**
+     * Used to communicate a cancel operation from the Whitebox GUI.
+     *
+     * @param cancel Set to true if the plugin should be canceled.
+     */
     @Override
     public void setCancelOp(boolean cancel) {
         cancelOp = cancel;
@@ -111,6 +172,12 @@ public class CreatePlane implements WhiteboxPlugin {
     }
     private boolean amIActive = false;
 
+    /**
+     * Used by the Whitebox GUI to tell if this plugin is still running.
+     *
+     * @return a boolean describing whether or not the plugin is actively being
+     * used.
+     */
     @Override
     public boolean isActive() {
         return amIActive;
--- RasterCreation/src/plugins/InterpolationIDW.java
+++ RasterCreation/src/plugins/InterpolationIDW.java
@@ -25,6 +25,7 @@ import whitebox.structures.KdTree;
 import java.io.*;
 
 /**
+ * WhiteboxPlugin is used to define a plugin tool for Whitebox GIS.
  *
  * @author Dr. John Lindsay <jlindsay@uoguelph.ca>
  */
@@ -33,33 +34,68 @@ public class InterpolationIDW implements WhiteboxPlugin {
     private WhiteboxPluginHost myHost = null;
     private String[] args;
     
+    /**
+     * Used to retrieve the plugin tool's name. This is a short, unique name
+     * containing no spaces.
+     *
+     * @return String containing plugin name.
+     */
     @Override
     public String getName() {
         return "InterpolationIDW";
     }
 
+    /**
+     * Used to retrieve the plugin tool's descriptive name. This can be a longer
+     * name (containing spaces) and is used in the interface to list the tool.
+     *
+     * @return String containing the plugin descriptive name.
+     */
     @Override
     public String getDescriptiveName() {
         return "Inverse Distance Weighted (IDW) Interpolation";
     }
 
+    /**
+     * Used to retrieve a short description of what the plugin tool does.
+     *
+     * @return String containing the plugin's description.
+     */
     @Override
     public String getToolDescription() {
         return "Interpolates XYZ point data from text files using an "
                 + "inverse-distance weighting.";
     }
 
+    /**
+     * Used to identify which toolboxes this plugin tool should be listed in.
+     *
+     * @return Array of Strings.
+     */
     @Override
     public String[] getToolbox() {
         String[] ret = {"Interpolation"};
         return ret;
     }
 
+    /**
+     * Sets the WhiteboxPluginHost to which the plugin tool is tied. This is the
+     * class that the plugin will send all feedback messages, progress updates,
+     * and return objects.
+     *
+     * @param host The WhiteboxPluginHost that called the plugin tool.
+     */
     @Override
     public void setPluginHost(WhiteboxPluginHost host) {
         myHost = host;
     }
 
+    /**
+     * Used to communicate feedback pop-up messages between a plugin tool and
+     * the main Whitebox user-interface.
+     *
+     * @param feedback String containing the text to display.
+     */
     private void showFeedback(String message) {
         if (myHost != null) {
             myHost.showFeedback(message);
@@ -68,6 +104,12 @@ public class InterpolationIDW implements WhiteboxPlugin {
         }
     }
 
+    /**
+     * Used to communicate a return object from a plugin tool to the main
+     * Whitebox user-interface.
+     *
+     * @return Object, such as an output WhiteboxRaster.
+     */
     private void returnData(Object ret) {
         if (myHost != null) {
             myHost.returnData(ret);
@@ -76,6 +118,13 @@ public class InterpolationIDW implements WhiteboxPlugin {
     private int previousProgress = 0;
     private String previousProgressLabel = "";
 
+    /**
+     * Used to communicate a progress update between a plugin tool and the main
+     * Whitebox user interface.
+     *
+     * @param progressLabel A String to use for the progress label.
+     * @param progress Float containing the progress value (between 0 and 100).
+     */
     private void updateProgress(String progressLabel, int progress) {
         if (myHost != null && ((progress != previousProgress)
                 || (!progressLabel.equals(previousProgressLabel)))) {
@@ -85,6 +134,12 @@ public class InterpolationIDW implements WhiteboxPlugin {
         previousProgressLabel = progressLabel;
     }
 
+    /**
+     * Used to communicate a progress update between a plugin tool and the main
+     * Whitebox user interface.
+     *
+     * @param progress Float containing the progress value (between 0 and 100).
+     */
     private void updateProgress(int progress) {
         if (myHost != null && progress != previousProgress) {
             myHost.updateProgress(progress);
@@ -92,12 +147,22 @@ public class InterpolationIDW implements WhiteboxPlugin {
         previousProgress = progress;
     }
 
+    /**
+     * Sets the arguments (parameters) used by the plugin.
+     *
+     * @param args
+     */
     @Override
     public void setArgs(String[] args) {
         this.args = args.clone();
     }
     private boolean cancelOp = false;
 
+    /**
+     * Used to communicate a cancel operation from the Whitebox GUI.
+     *
+     * @param cancel Set to true if the plugin should be canceled.
+     */
     @Override
     public void setCancelOp(boolean cancel) {
         cancelOp = cancel;
@@ -109,6 +174,12 @@ public class InterpolationIDW implements WhiteboxPlugin {
     }
     private boolean amIActive = false;
 
+    /**
+     * Used by the Whitebox GUI to tell if this plugin is still running.
+     *
+     * @return a boolean describing whether or not the plugin is actively being
+     * used.
+     */
     @Override
     public boolean isActive() {
         return amIActive;
