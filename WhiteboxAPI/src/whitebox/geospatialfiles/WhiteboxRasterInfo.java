20d5b707261637de19b1357215a119a9a533b12f
nirkas <nirkas@gmail.com>
2012-05-22 18:17:59 +0000

    GNU General Public License additions

--- WhiteboxAPI/src/whitebox/geospatialfiles/WhiteboxRasterInfo.java
+++ WhiteboxAPI/src/whitebox/geospatialfiles/WhiteboxRasterInfo.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2012 Dr. John Lindsay <jlindsay@uoguelph.ca>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package whitebox.geospatialfiles;
  
 
@@ -5,7 +22,7 @@ package whitebox.geospatialfiles;
  * The WhiteboxRasterInfo is a lightweight alternative to the WhiteboxRaster class and can be
  * used to manipulate Whitebox GAT raster header files (.dep). If you need to read or write data
  * to the grid cells contained in the raster, you should use the WhiteboxRaster class instead.
- * @author John Lindsay
+ * @author Dr. John Lindsay <jlindsay@uoguelph.ca>
  */
 public class WhiteboxRasterInfo extends WhiteboxRasterBase {
 
