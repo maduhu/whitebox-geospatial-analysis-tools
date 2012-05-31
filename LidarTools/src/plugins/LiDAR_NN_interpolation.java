b7a058e9122619f392b494e9279be18951e966b5
dr.john.lindsay <dr.john.lindsay@gmail.com>
2012-05-31 16:35:14 +0000

    Minor changes made to the LidarTools files

--- LidarTools/src/plugins/LiDAR_NN_interpolation.java
+++ LidarTools/src/plugins/LiDAR_NN_interpolation.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011 Dr. John Lindsay <jlindsay@uoguelph.ca>
+ * Copyright (C) 2011-2012 Dr. John Lindsay <jlindsay@uoguelph.ca>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -609,7 +609,7 @@ public class LiDAR_NN_interpolation implements WhiteboxPlugin {
 
 
 ///*
-// * Copyright (C) 2011 Dr. John Lindsay <jlindsay@uoguelph.ca>
+// * Copyright (C) 2011-2012 Dr. John Lindsay <jlindsay@uoguelph.ca>
 // *
 // * This program is free software: you can redistribute it and/or modify
 // * it under the terms of the GNU General Public License as published by
