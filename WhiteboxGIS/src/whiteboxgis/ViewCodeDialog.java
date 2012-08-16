2eef914920f6b38f5cb78f81adcb16cde2a3b81b
dr.john.lindsay <dr.john.lindsay@gmail.com>
2012-08-16 13:46:59 +0000

    minor changes to ViewCodeDialog and some help files

--- WhiteboxGIS/src/whiteboxgis/ViewCodeDialog.java
+++ WhiteboxGIS/src/whiteboxgis/ViewCodeDialog.java
@@ -151,10 +151,16 @@ public class ViewCodeDialog extends JFrame implements ActionListener {
             save.addActionListener(this);
             save.setVisible(editable);
 
-            JMenuItem close = new JMenuItem("Close");
+            JMenuItem saveAs = new JMenuItem("Save As");
+            FileMenu.add(saveAs);
+            saveAs.setActionCommand("saveAs");
+            saveAs.addActionListener(this);
+            saveAs.setVisible(editable);
+            
+            JMenuItem close = new JMenuItem("Quit");
             FileMenu.add(close);
-            close.setActionCommand("close");
-            close.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, 
+            close.setActionCommand("quit");
+            close.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Q, 
                     Toolkit.getDefaultToolkit().getMenuShortcutKeyMask()));
             close.addActionListener(this);
 
@@ -174,6 +180,71 @@ public class ViewCodeDialog extends JFrame implements ActionListener {
             } else if (files[x].toString().contains(pluginName + ".java")) {
                 sourceFile = files[x].toString();
                 break;
+            }
+        }
+    }
+    
+    private void saveAs() {
+            JFileChooser fc = new JFileChooser();
+        fc.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        fc.setMultiSelectionEnabled(false);
+        fc.setAcceptAllFileFilterUsed(true);
+        fc.setFileHidingEnabled(true);
+
+        FileFilter ft = new FileNameExtensionFilter("Text Files", "txt");
+        fc.addChoosableFileFilter(ft);
+        ft = new FileNameExtensionFilter("Whitebox Raster Files", "dep");
+        fc.addChoosableFileFilter(ft);
+
+        fc.setCurrentDirectory(new File(host.getWorkingDirectory()));
+        int result = fc.showSaveDialog(this);
+        File file = null;
+        if (result == JFileChooser.APPROVE_OPTION) {
+            file = fc.getSelectedFile();
+            if (file.exists()) {
+                Object[] options = {"Yes", "No"};
+                int n = JOptionPane.showOptionDialog(this,
+                        "The file already exists.\n"
+                        + "Would you like to overwrite it?",
+                        "Whitebox GAT Message",
+                        JOptionPane.YES_NO_OPTION,
+                        JOptionPane.QUESTION_MESSAGE,
+                        null, //do not use a custom Icon
+                        options, //the titles of buttons
+                        options[0]); //default button title
+
+                if (n == JOptionPane.YES_OPTION) {
+                    file.delete();
+                } else if (n == JOptionPane.NO_OPTION) {
+                    return;
+                }
+            }
+            sourceFile = file.toString();
+        } else {
+            return;
+        }
+        
+        file = new File(sourceFile);
+        FileWriter fw = null;
+        BufferedWriter bw = null;
+        PrintWriter out = null;
+        try {
+            fw = new FileWriter(file, false);
+            bw = new BufferedWriter(fw);
+            out = new PrintWriter(bw, true);
+
+            out.print(editor.getText());
+
+            bw.close();
+            fw.close();
+        } catch (java.io.IOException e) {
+            System.err.println("Error: " + e.getMessage());
+        } catch (Exception e) { //Catch exception if any
+            System.err.println("Error: " + e.getMessage());
+        } finally {
+            if (out != null || bw != null) {
+                out.flush();
+                out.close();
             }
         }
     }
@@ -249,11 +320,15 @@ public class ViewCodeDialog extends JFrame implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         Object source = e.getSource();
         String actionCommand = e.getActionCommand();
-        if (actionCommand.equals("close")) {
+        if (actionCommand.equals("quit")) {
             this.dispose();
         } else if (actionCommand.equals("save")) {
             if (editable) { 
                 save(); 
+            }
+        } else if (actionCommand.equals("saveAs")) {
+            if (editable) {
+                saveAs();
             }
         } else if (actionCommand.equals("open")) {
             
