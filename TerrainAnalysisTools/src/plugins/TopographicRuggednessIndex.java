c41a93f52523b8bf01ae610be48dda6de2edd36d
dr.john.lindsay <dr.john.lindsay@gmail.com>
2013-02-07 17:28:24 +0000

    Addition of a Topographic Ruggedness Index tool

--- TerrainAnalysisTools/src/plugins/TopographicRuggednessIndex.java
+++ TerrainAnalysisTools/src/plugins/TopographicRuggednessIndex.java
@@ -14,7 +14,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package plugins;
 
 import java.util.Date;
@@ -27,7 +26,7 @@ import whitebox.interfaces.WhiteboxPluginHost;
  *
  * @author Dr. John Lindsay <jlindsay@uoguelph.ca>
  */
-public class Slope implements WhiteboxPlugin {
+public class TopographicRuggednessIndex implements WhiteboxPlugin {
     
     private WhiteboxPluginHost myHost = null;
     private String[] args;
@@ -40,7 +39,7 @@ public class Slope implements WhiteboxPlugin {
      */
     @Override
     public String getName() {
-        return "Slope";
+        return "TopographicRuggednessIndex";
     }
 
     /**
@@ -51,7 +50,7 @@ public class Slope implements WhiteboxPlugin {
      */
     @Override
     public String getDescriptiveName() {
-    	return "Slope";
+    	return "Topographic Ruggedness Index";
     }
 
     /**
@@ -61,7 +60,7 @@ public class Slope implements WhiteboxPlugin {
      */
     @Override
     public String getToolDescription() {
-    	return "This tool calculates a slope grid from a digital elevation model (DEM).";
+    	return "Calculates the Riley, et al. (1999) ruggedness index.";
     }
 
     /**
@@ -71,7 +70,7 @@ public class Slope implements WhiteboxPlugin {
      */
     @Override
     public String[] getToolbox() {
-    	String[] ret = { "SurfDerivatives" };
+    	String[] ret = { "ElevResiduals" };
     	return ret;
     }
 
@@ -173,7 +172,7 @@ public class Slope implements WhiteboxPlugin {
     }
     
     private boolean amIActive = false;
-   
+    
     /**
      * Used by the Whitebox GUI to tell if this plugin is still running.
      *
@@ -191,22 +190,25 @@ public class Slope implements WhiteboxPlugin {
         
         String inputHeader = null;
         String outputHeader = null;
-        double zConvFactor = 1;
-    	
+        int row, col, x, y;
+        float progress = 0;
+        int i;
+        double zFactor = 1;
+        double z, zN;
+        int[] dX = new int[]{1, 1, 1, 0, -1, -1, -1, 0};
+        int[] dY = new int[]{-1, 0, 1, 1, 1, 0, -1, -1};
+        double SS;
+        int N;
+        double tri;
+        
         if (args.length <= 0) {
             showFeedback("Plugin parameters have not been set.");
             return;
         }
         
-        for (int i = 0; i < args.length; i++) {
-            if (i == 0) {
-                inputHeader = args[i];
-            } else if (i == 1) {
-                outputHeader = args[i];
-            } else if (i == 2) {
-                zConvFactor = Double.parseDouble(args[i]);
-            }
-        }
+        inputHeader = args[0];
+        outputHeader = args[1];
+        zFactor = Double.parseDouble(args[2]);
 
         // check to see that the inputHeader and outputHeader are not null.
         if ((inputHeader == null) || (outputHeader == null)) {
@@ -215,49 +217,34 @@ public class Slope implements WhiteboxPlugin {
         }
 
         try {
-            int row, col;
-            double z;
-            double[] N = new double[8];
-            double fx, fy;
-            float slope;
-            float progress = 0;
-            int[] Dy = {-1, 0, 1, 1, 1, 0, -1, -1};
-            int[] Dx = {1, 1, 1, 0, -1, -1, -1, 0};
-            final double radToDeg = 180 / Math.PI;
-
-            WhiteboxRaster inputFile = new WhiteboxRaster(inputHeader, "r");
-            inputFile.isReflectedAtEdges = true;
-
-            int rows = inputFile.getNumberRows();
-            int cols = inputFile.getNumberColumns();
-            double gridRes = inputFile.getCellSizeX();
-            double eightGridRes = 8 * gridRes;
-
-            double noData = inputFile.getNoDataValue();
-
-            WhiteboxRaster outputFile = new WhiteboxRaster(outputHeader, "rw", inputHeader, WhiteboxRaster.DataType.FLOAT, noData);
-            outputFile.setPreferredPalette("spectrum.pal");
-
+            WhiteboxRaster DEM = new WhiteboxRaster(inputHeader, "r");
+            
+            int rows = DEM.getNumberRows();
+            int cols = DEM.getNumberColumns();
+            double noData = DEM.getNoDataValue();
+            
+            WhiteboxRaster output = new WhiteboxRaster(outputHeader, "rw", inputHeader, WhiteboxRaster.DataType.FLOAT, noData);
+            output.setPreferredPalette("spectrum.pal");
+            output.setDataScale(WhiteboxRaster.DataScale.CONTINUOUS);
+            
             for (row = 0; row < rows; row++) {
                 for (col = 0; col < cols; col++) {
-                    z = inputFile.getValue(row, col);
+                    z = DEM.getValue(row, col);
                     if (z != noData) {
-                        for (int i = 0; i < 8; i++) {
-                            N[i] = inputFile.getValue(row + Dy[i], col + Dx[i]);
-                            if (N[i] != noData) {
-                                N[i] = N[i] * zConvFactor;
-                            } else {
-                                N[i] = z * zConvFactor;
+                        z = z * zFactor;
+                        N = 0;
+                        SS = 0;
+                        for (i = 0; i < 8; i++) {
+                            zN = DEM.getValue(row + dY[i], col + dX[i]);
+                            if (zN != noData) {
+                                zN = zN * zFactor;
+                                SS += (zN - z) * (zN - z);
+                                N++;
                             }
                         }
-                        fy = (N[6] - N[4] + 2 * (N[7] - N[3]) + N[0] - N[2]) / eightGridRes;
-                        fx = (N[2] - N[4] + 2 * (N[1] - N[5]) + N[0] - N[6]) / eightGridRes;
-                        slope = (float)(Math.atan(Math.sqrt(fx * fx + fy * fy)) * radToDeg);
-                        outputFile.setValue(row, col, slope);
-                    } else {
-                        outputFile.setValue(row, col, noData);
+                        tri = Math.sqrt(SS / N);
+                        output.setValue(row, col, tri);
                     }
-
                 }
                 if (cancelOp) {
                     cancelOperation();
@@ -267,12 +254,12 @@ public class Slope implements WhiteboxPlugin {
                 updateProgress((int) progress);
             }
 
-            outputFile.addMetadataEntry("Created by the "
+            output.addMetadataEntry("Created by the "
                     + getDescriptiveName() + " tool.");
-            outputFile.addMetadataEntry("Created on " + new Date());
-
-            inputFile.close();
-            outputFile.close();
+            output.addMetadataEntry("Created on " + new Date());
+            
+            DEM.close();
+            output.close();
 
             // returning a header file string displays the image.
             returnData(outputHeader);
