3532a186ec635159d357551a695835bbd1fe483b
dr.john.lindsay <dr.john.lindsay@gmail.com>
2013-04-26 18:06:21 +0000

    changes to help files

--- WhiteboxGIS/src/whiteboxgis/Help.java
+++ WhiteboxGIS/src/whiteboxgis/Help.java
@@ -20,8 +20,11 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Frame;
 import java.awt.event.*;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.text.DecimalFormat;
@@ -35,7 +38,6 @@ import javax.swing.event.HyperlinkEvent;
 import javax.swing.event.HyperlinkListener;
 import whitebox.interfaces.Communicator;
 import whitebox.utilities.FileUtilities;
-
 
 /**
  *
@@ -65,7 +67,7 @@ public class Help extends JDialog implements ActionListener, HyperlinkListener {
     private String activeTab = "index";
     private ArrayList<String> helpHistory = new ArrayList<String>();
     private int helpHistoryIndex = 0;
-    
+
     public Help(Frame owner, boolean modal, String startMode) {
         super(owner, modal);
         pathSep = File.separator;
@@ -89,6 +91,34 @@ public class Help extends JDialog implements ActionListener, HyperlinkListener {
         activeTab = startMode;
         createGui();
     }
+    
+    public Help() {
+        // this is really not to be used by anything other than the 'main' method for testing.
+        try {
+            pathSep = File.separator;
+
+            String applicationDirectory = java.net.URLDecoder.decode(getClass().getProtectionDomain().getCodeSource().getLocation().getPath(), "UTF-8");
+            //getClass().getProtectionDomain().
+            if (applicationDirectory.endsWith(".exe") || applicationDirectory.endsWith(".jar")) {
+                applicationDirectory = new File(applicationDirectory).getParent();
+            } else {
+                // Add the path to the class files
+                applicationDirectory += getClass().getName().replace('.', File.separatorChar);
+
+                // Step one level up as we are only interested in the
+                // directory containing the class files
+                applicationDirectory = new File(applicationDirectory).getParent();
+            }
+
+            resourcesDirectory = applicationDirectory + pathSep + "resources" + pathSep;
+            graphicsDirectory = resourcesDirectory + "Images" + pathSep;
+            helpDirectory = resourcesDirectory + "Help" + pathSep;
+            graphicsDirectory = resourcesDirectory + "Images" + pathSep;
+            helpFile = helpDirectory + "Welcome.html";
+        } catch (Exception e) {
+
+        }
+    }
 
     private void createGui() {
         try {
@@ -98,14 +128,14 @@ public class Help extends JDialog implements ActionListener, HyperlinkListener {
             this.setPreferredSize(new Dimension(950, 700));
 
             JTabbedPane sidebarPane = createSidePanel();
-            
+
             String imgLocation = null;
             ImageIcon image = null;
 
             helpPane.addHyperlinkListener(this);
             helpPane.setContentType("text/html");
             JScrollPane helpScroll = new JScrollPane(helpPane);
-        
+
             //helpPane = new FramePanel();
 
             Box box2 = Box.createHorizontalBox();
@@ -211,7 +241,6 @@ public class Help extends JDialog implements ActionListener, HyperlinkListener {
             availableHelpFiles.setModel(model);
 
             MouseListener ml = new MouseAdapter() {
-
                 @Override
                 public void mousePressed(MouseEvent e) {
                     JList theList = (JList) e.getSource();
@@ -238,7 +267,6 @@ public class Help extends JDialog implements ActionListener, HyperlinkListener {
                             //helpPane.navigate(helpFile);
                         } catch (MalformedURLException me) {
                         } catch (IOException ioe) {
-                            
                         }
                     }
                 }
@@ -254,8 +282,8 @@ public class Help extends JDialog implements ActionListener, HyperlinkListener {
             indexPanel.add(Box.createVerticalStrut(10));
             indexPanel.add(scroller1);
             sidebarTab.add(indexPanel, "Index");
-            
-            
+
+
             searchField.setMaximumSize(new Dimension(500, 20));
             searchField.addActionListener(searchFieldListener);
             searchPanel.setLayout(new BoxLayout(searchPanel, BoxLayout.Y_AXIS));
@@ -265,21 +293,21 @@ public class Help extends JDialog implements ActionListener, HyperlinkListener {
             JScrollPane scroller2 = new JScrollPane(searchOutput);
             searchPanel.add(scroller2);
             sidebarTab.add(searchPanel, "Search");
-            
+
             if (activeTab.toLowerCase().equals("index")) {
                 sidebarTab.setSelectedIndex(0);
             } else if (activeTab.toLowerCase().equals("search")) {
                 sidebarTab.setSelectedIndex(1);
             }
-                    
+
         } catch (Exception e) {
             System.out.println(e.getMessage());
         } finally {
             return sidebarTab;
         }
     }
-    
     private JPopupMenu helpPopup;
+
     private void createPopupMenus() {
         helpPopup = new JPopupMenu();
 
@@ -288,7 +316,7 @@ public class Help extends JDialog implements ActionListener, HyperlinkListener {
         mi.setActionCommand("viewHelpFileSource");
         helpPopup.add(mi);
     }
-    
+
     private void findAvailableHelpFiles() {
         try {
             ArrayList<String> allHelpFiles =
@@ -298,7 +326,6 @@ public class Help extends JDialog implements ActionListener, HyperlinkListener {
             helpFiles1 = allHelpFiles.toArray(helpFiles1);
 
             Arrays.sort(helpFiles1, new Comparator<String>() {
-
                 @Override
                 public int compare(String str1, String str2) {
                     return str1.toLowerCase().compareTo(str2.toLowerCase());
@@ -316,27 +343,88 @@ public class Help extends JDialog implements ActionListener, HyperlinkListener {
             System.out.println(e.getMessage());
         }
     }
-    
+
     private void back() {
-        if (helpHistoryIndex == 0) { return; }
+        if (helpHistoryIndex == 0) {
+            return;
+        }
         helpHistoryIndex--;
         try {
             helpPane.setPage("file:" + helpHistory.get(helpHistoryIndex));
         } catch (IOException e) {
             System.err.println(e.getStackTrace());
-        } 
-        
+        }
+
     }
-    
+
     private void forward() {
-        if (helpHistoryIndex == helpHistory.size() - 1) { return; }
+        if (helpHistoryIndex == helpHistory.size() - 1) {
+            return;
+        }
         helpHistoryIndex++;
         try {
             helpPane.setPage("file:" + helpHistory.get(helpHistoryIndex));
         } catch (IOException e) {
             System.err.println(e.getStackTrace());
-        } 
-        
+        }
+
+    }
+
+    private void createHelpTableOfContents() {
+        // This method is used to generate the table of contents html page
+        // used on the Whitebox GAT online help. It is not meant for general use.
+        findAvailableHelpFiles();
+        String outputFile = helpDirectory + "Help_TOC.html";
+        if (new File(outputFile).exists()) {
+            new File(outputFile).delete();
+        }
+        File file = new File(outputFile);
+        FileWriter fw = null;
+        BufferedWriter bw = null;
+        PrintWriter out = null;
+        try {
+            fw = new FileWriter(file, false);
+            bw = new BufferedWriter(fw);
+            out = new PrintWriter(bw, true);
+
+            String str;
+
+            str = "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\"><html>";
+            out.println(str);
+            str = "<head>";
+            out.println(str);
+            str = "<meta content=\"text/html; charset=iso-8859-1\" http-equiv=\"content-type\"><title>Help Topics</title>"
+                    + "<link rel=\"stylesheet\" type=\"text/css\" href=\"Help.css\"></meta>";
+            out.println(str);
+            str = "</head>";
+            out.println(str);
+            str = "<body><h1>Help Topics</h1>";
+            out.println(str);
+            str = "<p>";
+            out.println(str);
+            
+            for (int i = 0; i < helpFiles.length; i++) {
+                //model.add(i, helpFiles[i][1]);
+                str = "<a href=\"http://www.uoguelph.ca/~hydrogeo/Whitebox/Help/" + helpFiles[i][1] + ".html\" " +
+                "target=\"Body_Frame\">" + helpFiles[i][1] + "</a><br>";
+                out.println(str);
+            }
+            
+            str = "</p>";
+            out.println(str);
+
+            str = "</body>\n</html>";
+            out.println(str);
+
+        } catch (java.io.IOException e) {
+            System.err.println("Error: " + e.getMessage());
+        } finally {
+            if (out != null || bw != null) {
+                out.flush();
+                out.close();
+            }
+
+        }
     }
 
     @Override
@@ -350,14 +438,20 @@ public class Help extends JDialog implements ActionListener, HyperlinkListener {
         } else if (actionCommand.equals("forward")) {
             forward();
         } else if (actionCommand.equals("viewSource")) {
-            ViewCodeDialog vcd = new ViewCodeDialog((Frame)host, new File(helpFile), true);
+            ViewCodeDialog vcd = new ViewCodeDialog((Frame) host, new File(helpFile), true);
             vcd.setSize(new Dimension(800, 600));
             vcd.setVisible(true);
         }
     }
     
-    private ActionListener indexFieldListener = new ActionListener() {
+    // This method is only used during testing.
+    public static void main(String[] args) {
 
+        Help help = new Help();
+        help.createHelpTableOfContents();
+    }
+    
+    private ActionListener indexFieldListener = new ActionListener() {
         @Override
         public void actionPerformed(ActionEvent evt) {
             try {
@@ -365,22 +459,17 @@ public class Help extends JDialog implements ActionListener, HyperlinkListener {
                 helpPane.setPage("file:" + helpHistory.get(helpHistoryIndex));
             } catch (MalformedURLException me) {
             } catch (IOException ioe) {
-                
             }
         }
     };
-    
-    
     private ActionListener searchFieldListener = new ActionListener() {
-
         @Override
         public void actionPerformed(ActionEvent evt) {
             searchForWords();
-            
+
         }
     };
-    
-    
+
 //    private void helpMousePress(MouseEvent e) {
 //        //int selRow = layersTree.getRowForLocation(e.getX(), e.getY());
 //        //TreePath selPath = layersTree.getPathForLocation(e.getX(), e.getY());
@@ -389,15 +478,14 @@ public class Help extends JDialog implements ActionListener, HyperlinkListener {
 //            helpPopup.show((JComponent) e.getSource(), e.getX(), e.getY());
 //        }
 //    }
-    
     private void searchForWords() {
         try {
             DefaultListModel model = new DefaultListModel();
-            
+
             searchCounts = new int[helpFiles.length][2];
-            
+
             String line = searchField.getText().trim().toLowerCase();
-            
+
             // find quotations
             ArrayList<String> quotedStrings = new ArrayList<String>();
             Pattern p = Pattern.compile("\"([^\"]*)\"");
@@ -405,17 +493,17 @@ public class Help extends JDialog implements ActionListener, HyperlinkListener {
             while (m.find()) {
                 quotedStrings.add(m.group(1));
             }
-            
+
             // now remove all quotedStrings from the line
             for (int i = 0; i < quotedStrings.size(); i++) {
                 line = line.replace(quotedStrings.get(i), "");
             }
-            
+
             line = line.replace("\"", "");
-            
+
             int count = 0;
             int index = 0;
-            
+
             line = line.replace("-", " ");
             line = line.replace(" the ", "");
             line = line.replace(" a ", "");
@@ -425,11 +513,11 @@ public class Help extends JDialog implements ActionListener, HyperlinkListener {
             line = line.replace(" be ", "");
             line = line.replace(" in ", "");
             line = line.replace(" it ", "");
-            
+
             String[] words = line.split(" ");
-            
+
             int numWordsToMatch = words.length + quotedStrings.size();
-            
+
             for (int i = 0; i < helpFiles.length; i++) {
                 String fileContents = FileUtilities.readFileAsString(helpFiles[i][0]).toLowerCase();
                 fileContents = fileContents.replace("-", " ");
@@ -439,7 +527,7 @@ public class Help extends JDialog implements ActionListener, HyperlinkListener {
                         count++;
                     }
                 }
-                
+
                 for (String word : quotedStrings) {
                     if (fileContents.contains(word)) {
                         count++;
@@ -451,9 +539,8 @@ public class Help extends JDialog implements ActionListener, HyperlinkListener {
 
             }
 
-            
-            Arrays.sort(searchCounts, new Comparator<int[]>() {
 
+            Arrays.sort(searchCounts, new Comparator<int[]>() {
                 @Override
                 public int compare(int[] entry1, int[] entry2) {
                     final int int1 = entry1[0];
@@ -469,18 +556,17 @@ public class Help extends JDialog implements ActionListener, HyperlinkListener {
                     }
                 }
             });
-            
+
             DecimalFormat df = new DecimalFormat("0.0%");
             for (int i = 19; i >= 0; i--) {
                 if (searchCounts[i][0] > 0) {
-                    model.add(index, helpFiles[searchCounts[i][1]][1] + " (" + df.format((double)searchCounts[i][0] / numWordsToMatch) + ")");
+                    model.add(index, helpFiles[searchCounts[i][1]][1] + " (" + df.format((double) searchCounts[i][0] / numWordsToMatch) + ")");
                 }
             }
-            
+
             searchOutput.setModel(model);
 
             MouseListener ml = new MouseAdapter() {
-                
                 @Override
                 public void mousePressed(MouseEvent e) {
                     JList theList = (JList) e.getSource();
@@ -490,9 +576,9 @@ public class Help extends JDialog implements ActionListener, HyperlinkListener {
                         Object o = theList.getModel().getElementAt(index);
                         label = o.toString();
                     }
-                    if (e.getClickCount() == 1  && e.getButton() == 1) {
+                    if (e.getClickCount() == 1 && e.getButton() == 1) {
                         try {
-                            
+
                             helpFile = helpFiles[searchCounts[index][1]][0];
                             if (helpHistoryIndex == helpHistory.size() - 1) {
                                 helpHistory.add(helpFile);
@@ -504,11 +590,10 @@ public class Help extends JDialog implements ActionListener, HyperlinkListener {
                                 helpHistory.add(helpFile);
                                 helpHistoryIndex = helpHistory.size() - 1;
                             }
-                            
+
                             helpPane.setPage("file:" + helpHistory.get(helpHistoryIndex));
                         } catch (MalformedURLException me) {
                         } catch (IOException ioe) {
-                            
                         }
                     }
                 }
@@ -516,11 +601,9 @@ public class Help extends JDialog implements ActionListener, HyperlinkListener {
             searchOutput.addMouseListener(ml);
 
         } catch (IOException ioe) {
-            
         }
     }
-    
-    
+
     @Override
     public void hyperlinkUpdate(HyperlinkEvent event) {
         if (event.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
@@ -546,12 +629,10 @@ public class Help extends JDialog implements ActionListener, HyperlinkListener {
 
         @Override
         public void keyTyped(KeyEvent ke) {
-            
         }
 
         @Override
         public void keyPressed(KeyEvent ke) {
-
         }
 
         @Override
@@ -571,6 +652,5 @@ public class Help extends JDialog implements ActionListener, HyperlinkListener {
             }
             //throw new UnsupportedOperationException("Not supported yet.");
         }
-        
     }
 }
