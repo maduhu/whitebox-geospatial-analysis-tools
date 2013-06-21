bb9d54b85fa5963d5c23bca0df9b6889a3098eda
dr.john.lindsay <dr.john.lindsay@gmail.com>
2013-06-21 19:16:02 +0000

    minor change

--- ConversionTools/src/plugins/CoordinateSystemTransformation.java
+++ ConversionTools/src/plugins/CoordinateSystemTransformation.java
@@ -196,7 +196,7 @@ public class CoordinateSystemTransformation implements WhiteboxPlugin {
 
         returnData(new CoordinateTransformDialog());
 
-        
+
     }
 
     class CoordinateTransformDialog extends JPanel {
@@ -207,10 +207,37 @@ public class CoordinateSystemTransformation implements WhiteboxPlugin {
         private boolean myDialogFound = false;
         DialogFile dfIn;
         DialogFile dfOut;
-
         double a, b, f, bigE, esq, ePrimeSq, e1;
-        
+        double bigA;
+        double c;
+        double bigT;
+        double bigN;
+        double Mterm4;
+        double MTerm3;
+        double Mterm2;
+        double MTerm1;
+        double bigD;
+        double R1;
+        double n1;
+        double T1;
+        double C1;
+        double phi1;
+        double mu;
+        double bigM;
+        final double pi = Math.PI;
+        final double deg2Rad = pi / 180;
+        double easting, northing;
+        double lambdaNot;
+        double FN, FE;
+        double north, south, east, west;
+        double maxPhi, minNorthing, minEasting, maxEasting, maxNorthing, minPhi, minLambda, maxLambda;
+        String projectionDirection = "ll2utm";
+        boolean western, northern;
+        double phi, lambda;
+
         public CoordinateTransformDialog() {
+            test();
+            
             initUi();
         }
 
@@ -343,17 +370,27 @@ public class CoordinateSystemTransformation implements WhiteboxPlugin {
             ButtonGroup group = new ButtonGroup();
             group.add(ll2utmButton);
             group.add(utm2llButton);
-            
+
             //Register a listener for the radio buttons.
-            //ll2utmButton.addActionListener(this);
-            //utm2llButton.addActionListener(this);
-    
+            ll2utmButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    projectionDirection = "ll2utm";
+                }
+            });
+            utm2llButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    projectionDirection = "utm2ll";
+                }
+            });
+
             Box box1 = Box.createHorizontalBox();
             box1.add(ll2utmButton);
             box1.add(utm2llButton);
             mainBox.add(box1);
 
-            
+
             // N or S
             JRadioButton northButton = new JRadioButton("North");
             //LL2UTMButton.setMnemonic(KeyEvent.VK_R);
@@ -368,15 +405,43 @@ public class CoordinateSystemTransformation implements WhiteboxPlugin {
             ButtonGroup group2 = new ButtonGroup();
             group2.add(northButton);
             group2.add(southButton);
-            
+
             //Register a listener for the radio buttons.
             //ll2utmButton.addActionListener(this);
             //utm2llButton.addActionListener(this);
-    
+
             Box box2 = Box.createHorizontalBox();
             box2.add(northButton);
             box2.add(southButton);
             mainBox.add(box2);
+
+            // interpolation method
+            String[] zones = new String[60];
+            for (int a = 0; a < 60; a++) {
+                zones[a] = String.valueOf(a + 1);
+            }
+            ComboBoxProperty zoneChooser = new ComboBoxProperty(
+                    "Zone:", zones, 0);
+            zoneChooser.setName("zoneChooser");
+            ItemListener il3 = new ItemListener() {
+                @Override
+                public void itemStateChanged(ItemEvent e) {
+                    if (e.getStateChange() == ItemEvent.SELECTED) {
+                        Object item = e.getItem();
+                        String code;
+                        switch (item.toString().toLowerCase()) {
+                            case "chinese (china)":
+                                code = "zh_CN";
+                                break;
+
+                        }
+
+                    }
+                }
+            };
+            zoneChooser.setParentListener(il3);
+            zoneChooser.setBackColour(this.getBackground());
+            mainBox.add(zoneChooser);
 
             Box btnBox = Box.createHorizontalBox();
             btnBox.add(Box.createHorizontalGlue());
@@ -418,15 +483,82 @@ public class CoordinateSystemTransformation implements WhiteboxPlugin {
                 System.out.println(e.toString());
             }
         }
-        
+
+        private void test() {
+            double lat, lon, lon0, a, b, f, k0, e, e2, n, rho, nu, p;
+            double S, A0, B0, C0, D0, E0;
+            double tmp;
+            long zone;
+            
+            lat = 40.5 * deg2Rad;
+            lon = -73.5 * deg2Rad;
+            tmp = (-73.5 - (-180)) / 6;
+            zone = Math.round(tmp) + 1;
+            if (zone > tmp) {
+                zone = zone - 1;
+            }
+            lon0 = (-180 + zone * 6 - 3) * deg2Rad;
+            
+            // datum constants
+            a = ellipsoid.majorAxis();
+            b = ellipsoid.minorAxis();
+            f = (a - b) / a;
+            k0 = 0.9996;
+            e = Math.sqrt(1 - (b * b) / (a * a));
+            e2 = (e * e) / (1 - e * e);
+            n = (a - b) /(a + b);
+            rho = a * (1 - e * e) / (Math.pow((1 - (e * Math.sin(lat)) * (e * Math.sin(lat))), (3.0 / 2)));
+            nu = a / Math.sqrt((1 - (e * Math.sin(lat)) * (e * Math.sin(lat))));
+            
+            
+            // Calculate Meridional Arc Length
+            double n4 = Math.pow(n, 4);
+            A0 = a * (1 - n + (5 * n * n / 4.0) * (1 - n) +(81 * n4 / 64.0) * (1 - n));
+            B0 = (3 * a * n / 2.0) * (1 - n - (7 * n * n / 8.0) * (1 - n) + 55 * n4 / 64.0);
+            C0 = (15 * a * n * n / 16.0) * (1 - n +(3 * n * n / 4.0) * (1 - n));
+            D0 = (35 * a * Math.pow(n, 3) / 48.0) * (1 - n + 11 * n * n / 16.0);
+            E0 = (315 * a * n4 / 51.0) * (1 - n);
+            S = A0 * lat - B0 * Math.sin(2 * lat) + C0 * Math.sin(4 * lat) - D0 * Math.sin(6 * lat) + E0 * Math.sin(8 * lat);
+            
+            
+            
+            bigE = Math.sqrt((a * a - b * b) / (a * a));
+            esq = (a * a - b * b) / (a * a);
+            ePrimeSq = (bigE * bigE) / (1 - bigE * bigE);
+            e1 = (1 - Math.sqrt(1 - esq)) / (1 + Math.sqrt(1 - esq));
+
+            east = -73.5;
+            north = 40.5;
+            western = (east <= 0) ? true : false;
+            northern = (north >= 0) ? true : false;
+            tmp = (east - (-180)) / 6;
+            zone = Math.round(tmp) + 1;
+            if (zone > tmp) {
+                zone = zone - 1;
+            }
+            lambdaNot = (-180 + zone * 6 - 3) * deg2Rad; // in radians
+            FN = (northern) ? 0 : 10000000;
+            FE = 500000;
+
+            phi = north * deg2Rad;
+            lambda = east * deg2Rad;
+            ll2UTM(phi, lambda);
+            System.out.println("Easting: " + easting + "\tNorthing: " + northing);
+
+        }
+
         private void run() {
+            
+            double tmp;
+            long tmp1;
             // get the file names
             String inputFile = dfIn.getValue();
             String outputFile = dfOut.getValue();
-            
+
             // get the grid resolution
-            
-            
+
+
+
             // calculate ellipsoid parameters; a = major axis, b = minor axis
             a = ellipsoid.majorAxis();
             b = ellipsoid.minorAxis();
@@ -436,6 +568,8 @@ public class CoordinateSystemTransformation implements WhiteboxPlugin {
             ePrimeSq = bigE * bigE / (1 - bigE * bigE);
             e1 = (1 - Math.sqrt(1 - esq)) / (1 + Math.sqrt(1 - esq));
 
+            WhiteboxRaster input = new WhiteboxRaster(inputFile, "r");
+
 //            Image.HeaderFileName = InputFile
 //            Image.WriteChangesToFile = False
 //            totalCol = Image.NumberColumns
@@ -444,64 +578,171 @@ public class CoordinateSystemTransformation implements WhiteboxPlugin {
 //
 //            If File.Exists(OutputFile) Then File.Delete(OutputFile)
 //            If File.Exists(Replace(OutputFile, ".dep", ".tas")) Then File.Delete(Replace(OutputFile, ".dep", ".tas"))
-//
-//
-//            minEasting = Double.MaxValue
-//            maxEasting = Double.MinValue
-//            minNorthing = Double.MaxValue
-//            maxNorthing = Double.MinValue
-//
-//            east = Image.East
-//            west = Image.West
-//            south = Image.South
-//            north = Image.North
 
-            
-            showFeedback(inputFile);
+
+            minEasting = Double.POSITIVE_INFINITY;
+            maxEasting = Double.NEGATIVE_INFINITY;
+            minNorthing = Double.POSITIVE_INFINITY;
+            maxNorthing = Double.NEGATIVE_INFINITY;
+
+            east = input.getEast();
+            west = input.getWest();
+            south = input.getSouth();
+            north = input.getNorth();
+
+            if (projectionDirection.equals("ll2utm")) {
+
+                western = (east <= 0) ? true : false;
+                northern = (north >= 0) ? true : false;
+                tmp = (east - (-180)) / 6;
+                tmp1 = Math.round(tmp) + 1;
+                if (tmp1 > tmp) {
+                    tmp1 = tmp1 - 1;
+                }
+                lambdaNot = (-180 + tmp1 * 6 - 3) * deg2Rad; // in radians
+                FN = (northern) ? 0 : 10000000;
+                FE = 500000;
+
+
+                //Calculate the Eastings and northings of each of the four corners and find the min and max
+                phi = north * deg2Rad;
+                lambda = west * deg2Rad;
+                ll2UTM(phi, lambda);
+                if (easting < minEasting) {
+                    minEasting = easting;
+                }
+                if (northing < minNorthing) {
+                    minNorthing = northing;
+                }
+                if (easting > maxEasting) {
+                    maxEasting = easting;
+                }
+                if (northing > maxNorthing) {
+                    maxNorthing = northing;
+                }
+
+                phi = north * deg2Rad;
+                lambda = east * deg2Rad;
+                ll2UTM(phi, lambda);
+                if (easting < minEasting) {
+                    minEasting = easting;
+                }
+                if (northing < minNorthing) {
+                    minNorthing = northing;
+                }
+                if (easting > maxEasting) {
+                    maxEasting = easting;
+                }
+                if (northing > maxNorthing) {
+                    maxNorthing = northing;
+                }
+
+                phi = south * deg2Rad;
+                lambda = east * deg2Rad;
+                ll2UTM(phi, lambda);
+                if (easting < minEasting) {
+                    minEasting = easting;
+                }
+                if (northing < minNorthing) {
+                    minNorthing = northing;
+                }
+                if (easting > maxEasting) {
+                    maxEasting = easting;
+                }
+                if (northing > maxNorthing) {
+                    maxNorthing = northing;
+                }
+
+                phi = south * deg2Rad;
+                lambda = west * deg2Rad;
+                ll2UTM(phi, lambda);
+                if (easting < minEasting) {
+                    minEasting = easting;
+                }
+                if (northing < minNorthing) {
+                    minNorthing = northing;
+                }
+                if (easting > maxEasting) {
+                    maxEasting = easting;
+                }
+                if (northing > maxNorthing) {
+                    maxNorthing = northing;
+                }
+
+//                newTotalCol = (maxEasting - minEasting) / SpecifiedGridRes
+//                newTotalRow = (maxNorthing - minNorthing) / SpecifiedGridRes
+//
+//                Output.HeaderFileName = OutputFile
+//                Output.WriteChangesToFile = True
+//                With Output
+//                    .East = maxEasting
+//                    .GridResolution = SpecifiedGridRes
+//                    .North = maxNorthing
+//                    .NumberColumns = newTotalCol
+//                    .NumberRows = newTotalRow
+//                    If northern Then
+//                        .Projection = "UTM" & " " & tmp1.ToString & "N"
+//                    Else
+//                        .Projection = "UTM" & " " & tmp1.ToString & "S"
+//                    End If
+//                    .South = minNorthing
+//                    .West = minEasting
+//                    .XYUnits = "meters"
+//                    .ZUnits = Image.ZUnits
+//                    .DataType = Image.DataType
+//                    .DataScale = Image.DataScale
+//                    .PreferredPalette = Image.PreferredPalette
+//                    .SetBlockData()
+//                    .InitializeGrid(newTotalCol, newTotalRow)
+//                End With
+//
+//                If cbResample.SelectedItem.ToString = "Nearest neighbour" Then
+//                    nearestNeighbor_ll2UTM()
+//                Else
+//                    Bilinear_ll2UTM()
+//                End If
+
+
+            }
+
             amIActive = false;
         }
-        
-        double  bigA;
-    double  c;
-    double  bigT;
-    double  bigN;
-    double Mterm4;
-    double  MTerm3;
-    double  Mterm2;
-    double  MTerm1;
-    private void ll2UTM(double phi, double lambda) {
-        try {
-            /*from Snyder (1987)
-            phi and lambda should be in radians
-            note: phi = latitude, lambda = longitude
-            */
-            
-            MTerm1 = (1 - (esq / 4) - (3 * bigE * bigE * bigE * bigE / 64) - (5 * 
-                    bigE * bigE * bigE * bigE * bigE * bigE / 256)) * phi;
-            Mterm2 = ((3 * esq / 8) + (3 * bigE * bigE * bigE * bigE / 32) + 45 * (
-                    bigE * bigE * bigE * bigE * bigE * bigE / 1024)) * Math.sin(2 * phi);
-            MTerm3 = ((15 * bigE  bigE * bigE * bigE / 256) + (45 * 
-                     bigE * bigE * bigE * bigE * bigE * bigE / 1024)) * Math.sin(4 * phi);
-            Mterm4 = (35 * bigE ^ 6 / 3072) * Math.Sin(6 * phi);
-//            bigM = a * (MTerm1 - Mterm2 + MTerm3 - Mterm4);
-//            If phi <> pi / 2 OrElse phi = -pi / 2 Then
-//                bigN = a / (1 - esq * (Math.Sin(phi)) ^ 2) ^ 0.5
-//                bigT = (Math.Tan(phi)) ^ 2
-//                c = ePrimeSq * (Math.Cos(phi)) ^ 2
-//                bigA = (lambda - lambdaNot) * Math.Cos(phi)
-//                easting = FE + 0.9996 * bigN * (bigA + (1 - bigT + c) * bigA ^ 3 / 6 + (5 - 18 * bigT + bigT ^ 2 + 72 * c - 58 * ePrimeSq) * bigA ^ 5 / 120)
-//                northing = FN + 0.9996 * (bigM - 0 + bigN * Math.Tan(phi) * (bigA ^ 2 / 2 + (5 - bigT + 9 * c + 4 * c ^ 2) * bigA ^ 4 / 24 + (61 - 58 * bigT + bigT ^ 2 + 600 * c - 330 * ePrimeSq) * bigA ^ 6 / 720))
-//            Else
-//                easting = FE
-//                northing = FN + 0.9996 * bigM
-//            End If
-                    } catch (Exception e) {
-                    showFeedback(e.toString());
-                    }
-    }
 
-        
-        
+        private void ll2UTM(double phi, double lambda) {
+            try {
+                /*from Snyder (1987)
+                 phi and lambda should be in radians
+                 note: phi = latitude, lambda = longitude
+                 */
+
+                MTerm1 = (1 - (esq / 4) - (3 * bigE * bigE * bigE * bigE / 64) - (5
+                        * bigE * bigE * bigE * bigE * bigE * bigE / 256)) * phi;
+                Mterm2 = ((3 * esq / 8) + (3 * bigE * bigE * bigE * bigE / 32) + 45 * (bigE * bigE * bigE * bigE * bigE * bigE / 1024)) * Math.sin(2 * phi);
+                MTerm3 = ((15 * bigE * bigE * bigE * bigE / 256)
+                        + (45 * bigE * bigE * bigE * bigE * bigE * bigE / 1024)) * Math.sin(4 * phi);
+                Mterm4 = (35 * bigE * bigE * bigE * bigE * bigE * bigE / 3072) * Math.sin(6 * phi);
+                bigM = a * (MTerm1 - Mterm2 + MTerm3 - Mterm4);
+                if ((phi != (pi / 2)) || (phi == (-pi / 2))) {
+                    bigN = Math.sqrt(a / (1 - esq * (Math.sin(phi) * Math.sin(phi))));
+                    bigT = (Math.tan(phi) * Math.tan(phi));
+                    c = ePrimeSq * (Math.cos(phi) * Math.cos(phi));
+                    bigA = (lambda - lambdaNot) * Math.cos(phi);
+                    easting = FE + 0.9996 * bigN * (bigA + (1 - bigT + c)
+                            * (bigA * bigA * bigA) / 6 + (5 - 18 * bigT + (bigT * bigT)
+                            + 72 * c - 58 * ePrimeSq) * (bigA * bigA * bigA * bigA * bigA) / 120);
+                    northing = FN + 0.9996 * (bigM - 0 + bigN * Math.tan(phi)
+                            * ((bigA * bigA) / 2 + (5 - bigT + 9 * c + 4 * (c * c))
+                            * (bigA * bigA * bigA * bigA) / 24 + (61 - 58 * bigT
+                            + (bigT * bigT) + 600 * c - 330 * ePrimeSq)
+                            * (bigA * bigA * bigA * bigA * bigA * bigA) / 720));
+                } else {
+                    easting = FE;
+                    northing = FN + 0.9996 * bigM;
+                }
+            } catch (Exception e) {
+                showFeedback(e.toString());
+            }
+        }
     }
 
     public static void main(String[] args) {
