9699d6b5367bc2ab8d60a331ced263fcb0aa2fc4
dr.john.lindsay <dr.john.lindsay@gmail.com>
2013-07-11 17:12:21 +0000

    minor change to ImageAutocorrelation

--- StatsTools/src/plugins/ImageAutocorrelation.java
+++ StatsTools/src/plugins/ImageAutocorrelation.java
@@ -16,6 +16,7 @@
  */
 package plugins;
 
+import org.apache.commons.math3.distribution.NormalDistribution;
 import java.text.DecimalFormat;
 import whitebox.geospatialfiles.WhiteboxRaster;
 import whitebox.interfaces.WhiteboxPlugin;
@@ -245,7 +246,10 @@ public class ImageAutocorrelation implements WhiteboxPlugin {
             double[] varRandomization = new double[numImages];
             double[] zN = new double[numImages];
             double[] zR = new double[numImages];
+            double[] pValueN = new double[numImages];
+            double[] pValueR = new double[numImages];
             double[] data;
+            NormalDistribution distribution = new NormalDistribution(0, 1);
 
             for (a = 0; a < numImages; a++) {
                 progressMessage = "Image " + (a + 1) + " of " + numImages;
@@ -325,6 +329,7 @@ public class ImageAutocorrelation implements WhiteboxPlugin {
                         ((W * W) * (n[a] * n[a] - 1));
                 
                 zN[a] = (I[a] - E_I[a]) / (Math.sqrt(varNormality[a])); 
+                pValueN[a] = 2d * (1.0 - distribution.cumulativeProbability(Math.abs(zN[a])));
                 
                 k = k / (n[a] * stdDev[a] * stdDev[a] * stdDev[a] * stdDev[a]);
                 
@@ -333,10 +338,10 @@ public class ImageAutocorrelation implements WhiteboxPlugin {
                         ((n[a] - 1) * (n[a] - 2) * (n[a] - 3) * W * W);
                 
                 zR[a] = (I[a] - E_I[a]) / (Math.sqrt(varRandomization[a])); 
+                pValueR[a] = 2d * (1.0 - distribution.cumulativeProbability(Math.abs(zR[a])));
                 
                 image.close();
                 
-
                 progress = (int) (100f * (a + 1) / numImages);
                 updateProgress(progressMessage, progress);
 
@@ -357,16 +362,18 @@ public class ImageAutocorrelation implements WhiteboxPlugin {
                     retstr.append("Mean of cells included:\t\t").append(df2.format(mean[a])).append(" ").append(units[a]).append("\n");
                 }
                 retstr.append("Spatial autocorrelation (Moran's I):\t").append(df2.format(I[a])).append("\n");
-                retstr.append("Expected value:\t").append(df2.format(E_I[a])).append("\n");
+                retstr.append("Expected value:\t\t").append(df2.format(E_I[a])).append("\n");
                 retstr.append("Variance of I (normality assumption):\t").append(df2.format(varNormality[a])).append("\n");
                 retstr.append("z test stat (normality assumption):\t").append(df2.format(zN[a])).append("\n");
+                retstr.append("p-value (normality assumption):\t").append(df2.format(pValueN[a])).append("\n");
                 retstr.append("Variance of I (randomization assumption):\t").append(df2.format(varRandomization[a])).append("\n");
                 retstr.append("z test stat (randomization assumption):\t").append(df2.format(zR[a])).append("\n");
+                retstr.append("p-value (randomization assumption):\t").append(df2.format(pValueR[a])).append("\n");
                 
             }
 
 
-            //System.out.println(retstr.toString());
+//            System.out.println(retstr.toString());
 
             returnData(retstr.toString());
 
