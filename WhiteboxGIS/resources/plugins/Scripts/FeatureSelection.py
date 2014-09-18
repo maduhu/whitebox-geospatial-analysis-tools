4fbe91f67fd9a566e431138f2a324889d6fe367a
dr.john.lindsay <dr.john.lindsay@gmail.com>
2014-09-18 19:31:27 +0000

    small change to FeatureSelection script

--- WhiteboxGIS/resources/plugins/Scripts/FeatureSelection.py
+++ WhiteboxGIS/resources/plugins/Scripts/FeatureSelection.py
@@ -15,7 +15,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
  
- import os
+import os
 from threading import Thread
 from whitebox.ui.plugin_dialog import ScriptDialog
 from java.awt.event import ActionListener
